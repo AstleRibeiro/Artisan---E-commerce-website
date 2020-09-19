285725f3a95fc43bdb1a745899c656f8f081756c
Astle <astleribeiro@gmail.com>
2020-09-19 23:22:20 +0530

    12 updated version

--- firsthello/views.py
+++ firsthello/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .forms import FormName
 from django.urls import reverse_lazy
+from dynamicnav.models import AbstractPainting
 
 
 def index(request):
     return render(request, 'splash.html')
 
 def home(request):
-    return render(request, 'index.html')
+    abs_painting = AbstractPainting.objects.all()
+    return render(request, 'index.html', {"abs_painting": abs_painting})
 
 
 
